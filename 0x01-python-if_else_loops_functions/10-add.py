5568b386cc594683dab0b1c47d88d121f3402097
Ousman Ahmed <ousman-ahmed.com>
2022-12-01 10:18:25 +0300

    task 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# Author -ous
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+# Author - ous
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
